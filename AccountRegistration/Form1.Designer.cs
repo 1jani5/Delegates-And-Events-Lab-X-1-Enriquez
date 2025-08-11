b3b0113df9c983316c9ce836c9903eadc2e4532b
1jani5 <Johnarvinenriquez@gmail.com>
2025-08-11 23:37:06 +0800

    hanngang number 5 ang natapos

--- AccountRegistration/Form1.Designer.cs
+++ AccountRegistration/Form1.Designer.cs
@@ -43,11 +43,15 @@
             textBox5 = new TextBox();
             textBox6 = new TextBox();
             comboBox1 = new ComboBox();
+            textBox7 = new TextBox();
+            button1 = new Button();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
+            label1.BackColor = SystemColors.MenuBar;
+            label1.ForeColor = SystemColors.ControlText;
             label1.Location = new Point(12, 26);
             label1.Name = "label1";
             label1.Size = new Size(70, 15);
@@ -58,6 +62,7 @@
             // label2
             // 
             label2.AutoSize = true;
+            label2.BackColor = SystemColors.MenuBar;
             label2.Location = new Point(195, 26);
             label2.Name = "label2";
             label2.Size = new Size(56, 15);
@@ -68,6 +73,7 @@
             // label3
             // 
             label3.AutoSize = true;
+            label3.BackColor = SystemColors.MenuBar;
             label3.Location = new Point(12, 87);
             label3.Name = "label3";
             label3.Size = new Size(66, 15);
@@ -78,6 +84,7 @@
             // label4
             // 
             label4.AutoSize = true;
+            label4.BackColor = SystemColors.MenuBar;
             label4.Location = new Point(196, 87);
             label4.Name = "label4";
             label4.Size = new Size(67, 15);
@@ -88,6 +95,7 @@
             // label5
             // 
             label5.AutoSize = true;
+            label5.BackColor = SystemColors.MenuBar;
             label5.Location = new Point(378, 87);
             label5.Name = "label5";
             label5.Size = new Size(82, 15);
@@ -97,6 +105,7 @@
             // label6
             // 
             label6.AutoSize = true;
+            label6.BackColor = SystemColors.MenuBar;
             label6.Location = new Point(12, 154);
             label6.Name = "label6";
             label6.Size = new Size(31, 15);
@@ -106,6 +115,7 @@
             // label7
             // 
             label7.AutoSize = true;
+            label7.BackColor = SystemColors.MenuBar;
             label7.Location = new Point(192, 154);
             label7.Name = "label7";
             label7.Size = new Size(71, 15);
@@ -115,6 +125,7 @@
             // label8
             // 
             label8.AutoSize = true;
+            label8.BackColor = SystemColors.MenuBar;
             label8.Location = new Point(12, 221);
             label8.Name = "label8";
             label8.Size = new Size(52, 15);
@@ -171,11 +182,32 @@
             comboBox1.Size = new Size(121, 23);
             comboBox1.TabIndex = 14;
             // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(12, 239);
+            textBox7.Multiline = true;
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(536, 96);
+            textBox7.TabIndex = 15;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(218, 363);
+            button1.Name = "button1";
+            button1.Size = new Size(83, 34);
+            button1.TabIndex = 16;
+            button1.Text = "Next";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.SlateGray;
             ClientSize = new Size(573, 450);
+            Controls.Add(button1);
+            Controls.Add(textBox7);
             Controls.Add(comboBox1);
             Controls.Add(textBox6);
             Controls.Add(textBox5);
@@ -214,5 +246,7 @@
         private TextBox textBox5;
         private TextBox textBox6;
         private ComboBox comboBox1;
+        private TextBox textBox7;
+        private Button button1;
     }
 }
--- AccountRegistration/Form1.cs
+++ AccountRegistration/Form1.cs
@@ -26,5 +26,10 @@ namespace AccountRegistration
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
