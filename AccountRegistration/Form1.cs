8089b279e3b3a64ba9a1c3fc818ca2cc33e9ff08
1jani5 <Johnarvinenriquez@gmail.com>
2025-08-11 19:30:29 +0800

    UI Created

--- AccountRegistration/Form1.cs
+++ AccountRegistration/Form1.cs
@@ -6,5 +6,25 @@ namespace AccountRegistration
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
